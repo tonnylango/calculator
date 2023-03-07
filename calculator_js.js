45da28df99155ee482b6defa4a837c967fe8c062
Jinamizi <tonnylango@gmail.com>
2023-03-08 01:54:00 +0300

    update

--- calculator_js.js
+++ calculator_js.js
@@ -1,22 +1,26 @@
-var question = 0; //should hold the question displayed
-var answer = 0;//hold the answer displayed
-
-
 $(document).ready(function(){
+	var question = 0; //should hold the question displayed
+	var answer = 0;//hold the answer displayed
+
 	//define function to update the display
 	function updateDisplay(){
-		let output = '${question}\n${answer}';
+		let output = `${question}\n${answer}`;
 		$(".display").text(output); //update the display
 	}
 
+	updateDisplay();
+
 	//if you click a number button, update question
-	$(".button").click(function(){
+	$(".number").click(function(){
 		let typedDigit = $(this).text();
-		alert('you pressed ${typedDigit}');
-		/*let typedDigit = $(this).text();
-		question = (question == "0") ? typedDigit : '${question}${typedDigit}';
-		$(".display").text(question)*/
-		//$("#display").updateDisplay(); //update the display
+		question = (question == "0") ? typedDigit : `${question}${typedDigit}`;
+		updateDisplay(); //update the display
+	});
+
+	$(".clear").click(function(){
+		question = 0;
+		answer = 0;
+		updateDisplay();
 	});
 	
 });
