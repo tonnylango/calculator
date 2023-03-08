d6a69054113ab51ed805cc8a16449dd60fefd01e
Jinamizi <tonnylango@gmail.com>
2023-03-09 00:14:29 +0300

    update

--- calculator_js.js
+++ calculator_js.js
@@ -1,6 +1,8 @@
 $(document).ready(function(){
-	var question = 0; //should hold the question displayed
-	var answer = 0;//hold the answer displayed
+	var question = "0"; //should hold the question displayed
+	var answer = "0";//hold the answer displayed
+	var number = "0"; //hold the number being typed
+	var operator; //holds latest operator
 
 	//define function to update the display
 	function updateDisplay(){
@@ -10,16 +12,38 @@ $(document).ready(function(){
 
 	updateDisplay();
 
+	/*
 	//if you click a number button, update question
 	$(".number").click(function(){
 		let typedDigit = $(this).text();
 		question = (question == "0") ? typedDigit : `${question}${typedDigit}`;
 		updateDisplay(); //update the display
-	});
+	});*/
+
+	$(".button").click(function(){
+		let typedDigit = $(this).text()
+		if ($.isNumeric(typedDigit)){
+			question = (question == "0") ? typedDigit : `${question}${typedDigit}`;
+			number = (number == "0") ? typedDigit : `${number}${typedDigit}`
+		}
+		else if (typedDigit == "." && !number.includes(".")){
+			question = `${question}${typedDigit}`;
+			number = number + typedDigit;
+		}
+		else if (typedDigit == "<="){ //deleting
+			question = question.slice(0, -1);
+			if (question.length == 0){
+				question = "0"
+			}
+		}
+
+		updateDisplay(); //update the display
+
+
+	})
 
 	$(".clear").click(function(){
-		question = 0;
-		answer = 0;
+		question = answer = number = "0";
 		updateDisplay();
 	});
 	
